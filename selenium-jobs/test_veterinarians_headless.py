8dd1c53f4ffcef5742e9a5bdc4e959e3a15baaf0
MdurmazGithub <mdurmaz7579@gmail.com>
2023-02-07 12:32:26 +0000

    mmm

--- selenium-jobs/test_veterinarians_headless.py
+++ selenium-jobs/test_veterinarians_headless.py
@@ -13,8 +13,7 @@ chrome_options.add_argument("disable-dev-shm-usage")
 
 # Update webdriver instance of chrome-driver with adding chrome options
 driver = webdriver.Chrome(options=chrome_options)
-
-# Connect to the application
+#
 APP_IP = os.environ['MASTER_PUBLIC_IP']
 url = "http://"+APP_IP.strip()+":30001/"
 print(url)
